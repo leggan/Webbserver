329212d9c8ea80ecd94dd8f07292b5ba3506d46b
leggan <203560373+xweblit@users.noreply.github.com>
2026-01-22 19:02:44 +0100

    Påbörjat med att fixa historik osv..

--- Lyckoautomaten/server.js
+++ Lyckoautomaten/server.js
@@ -1,25 +1,53 @@
 const express = require('express')
 const app = express()
+const session = require("express-session")
+const dotenv = require("dotenv").config()
+
+
+
 
 app.use(express.static('public'));
+app.use(session({
+    secret: process.env.SECRET,
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+        maxAge: 60000
+    }
+}))
 
 app.get("/api/lucky", (req, res) => {
+    if(!req.session.totalData) {
+        req.session.totalData = []
+    }
     const minNumber = 0
     const maxNumber = 100
     const randomNumber = Math.floor(Math.random() * maxNumber)
     const date = new Date().toString()
-    res.json({
+    const data = {
         "number" : randomNumber,
         "min" : minNumber,
         "max" : maxNumber,
         "date": date
-    })
+    }
+    if(req.session.totalData.length < 10) {
+        req.session.totalData.push(data)
+    } else {
+        req.session.totalData.pop()
+        req.session.totalData.push(data)
+    }
+    res.json(data)
+    console.log(JSON.stringify(req.session.totalData, null, 2))
+    
+    
 })
 
 app.get("/", (req, res) => {
-    res.render("index.html")
+    req.session.totalData = []
+    
+    console.log("Crazy")
 })
 
-app.listen(3000, () => {
+app.listen(process.env.SERVER_PORT, () => {
     console.log("Running")
 })
